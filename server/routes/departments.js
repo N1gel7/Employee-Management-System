fa96a95b5d007f74bfef2626bdc1e1b80a72e312
n1gel7 <nigelnortey123@gmail.com>
2025-09-24 00:12:49 +0000

    chnaged git ignore

--- server/routes/departments.js
+++ server/routes/departments.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const mysql = require("mysql");
 require('dotenv').config();
-
+//done
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
